03f34ab5d0a97ceba4846da817aa03028ed991f2
Lavaman491 <79179305+Lavaman491@users.noreply.github.com>
2021-02-18 23:08:40 -0500

    Looped and Added Front-End Instructions
    I added a while loop that loops through the input and output. To exit the loop, the user can type in either "quit" (any case) or leave the line blank. This is explained at the start, second line on the front end. Above that, a line explains the purpose of the program.

--- LengthOFWord.py
+++ LengthOFWord.py
@@ -1,4 +1,9 @@
 # Gives back the length of a given word
-word = input(">")
 
-print(len(word))
+print("-Finding the Length of a given word/sentence-")
+print('-Leave Blank or Type "quit" to Exit-')
+word = input("\n>")
+
+while word.lower() != "quit" and word != "": 
+  print(len(word))
+  word = input("\n>")
